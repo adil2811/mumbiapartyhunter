590d4a67fec9655ad4bddff6d7e56f5f1dc13aa0
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-12-27 17:50:26 +0530

    Update route.js

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -5,7 +5,9 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   connect();
   try {
-    const searchParams = request.nextUrl.searchParams || '';
+    const url = new URL(request.url);
+
+    const searchParams = url.searchParams ;
     const searchQuery = searchParams.get('searchQuery');
 
     let events;
