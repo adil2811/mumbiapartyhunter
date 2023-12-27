7995bf451c88f355d50a59a0c55a7b7c657efea7
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-12-27 13:41:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-        images: {
-          domains: ['adil-next-ecommerce.s3.amazonaws.com','utfs.io'],
-        },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'adil-next-ecommerce.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+    ],
+  },
+  webpack: (config) => {
+    // Modify webpack configuration to adjust module resolution fallback
+    config.resolve.fallback = {
+      "mongodb-client-encryption": false,
+      "aws4": false
+    };
 
-module.exports = nextConfig
+    // Return the updated configuration
+    return config;
+  },
+};
+
+module.exports = nextConfig;
