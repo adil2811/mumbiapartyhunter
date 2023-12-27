a95175e58c03fe45064475de1cebc660cc006950
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-12-27 21:33:49 +0530

    Update route.js

--- src/app/api/search/[search]/route.js
+++ src/app/api/search/[search]/route.js
@@ -2,21 +2,16 @@ import Event from "../../../../models/Event";
 import { connect } from "@/dbConfig/dbConfig";
 import { NextResponse } from "next/server";
 
-
-
-
-export async function GET(request,response) {
+export async function GET(request, response) {
   connect();
   try {
-    
-    console.log(response.params.search)
     const { search } = response.params; // Use request.params instead of response.params
-console.log('✌️search --->', search);
-const splitSearch = search.split('=');
-const searchQuery = splitSearch[1]; // Getting the value after '='
 
+    console.log('✌️search --->', search);
+    const splitSearch = search ? search.split('=') : [];
+    const searchQuery = splitSearch.length > 1 ? splitSearch[1] : undefined; // Getting the value after '='
 
-        console.log('✌️searchQuery --->', searchQuery);
+    console.log('✌️searchQuery --->', searchQuery);
 
     let events;
 
@@ -44,8 +39,8 @@ const searchQuery = splitSearch[1]; // Getting the value after '='
       });
     }
 
+    console.log('✌️events --->', events);
     return NextResponse.json(events); // Return a new Response object
-console.log('✌️events --->', events);
   } catch (error) {
     console.error(error);
     // Handle errors gracefully
